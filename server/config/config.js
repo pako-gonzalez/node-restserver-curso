9b43155a82ef1777f55aae86141cf74a1176e6a0
Francisco Jose Gonzalez Lozano <francisko.gonzalez@gmail.com>
2018-08-01 21:31:20 +0200

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,21 @@
 // Puerto
 // ========
 process.env.PORT = process.env.PORT || 3000;
+
+// ========
+// Entorno
+// ========
+process.env.NODE_ENV = process.env.NODE_ENV ||  'dev';
+
+// ========
+// BBDD
+// ========
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:123ABC@ds263571.mlab.com:63571/cafe';
+}
+
+process.env.URL_DB = urlDB;
